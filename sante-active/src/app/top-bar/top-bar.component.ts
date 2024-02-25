3483bd4451572f6d23ba282d00c5e2e782918c90
NoureddineHelb <80421386+NoureddineHelb@users.noreply.github.com>
2024-02-25 22:51:04 +0100

    :feat: responsive navbar

--- sante-active/src/app/top-bar/top-bar.component.ts
+++ sante-active/src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent {
+  @ViewChild('menuBtn') menuBtn!: ElementRef;
+  @ViewChild('navbar') navbar!: ElementRef;
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.menuBtn.nativeElement.addEventListener('click', () => this.toggleMenu());
+  }
+
+  toggleMenu() {
+    this.menuBtn.nativeElement.classList.toggle('fa-times');
+    this.navbar.nativeElement.classList.toggle('active');
+  }
 }
