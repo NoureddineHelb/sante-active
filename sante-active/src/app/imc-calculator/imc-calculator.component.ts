79e4e2380abfb1c9718aec77b7845b6d551ad99d
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2024-02-25 22:08:42 +0100

    :feat: add components

---
+++ sante-active/src/app/imc-calculator/imc-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imc-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './imc-calculator.component.html',
+  styleUrl: './imc-calculator.component.css'
+})
+export class ImcCalculatorComponent {
+
+}
