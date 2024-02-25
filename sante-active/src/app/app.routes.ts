370f9c3b95272ce21de786093e75539515d61b4a
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2024-02-25 22:09:18 +0100

    :fix: structure & css

--- sante-active/src/app/app.routes.ts
+++ sante-active/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {ServiceListComponent} from "./service-list/service-list.component";
+import {ImcCalculatorComponent} from "./imc-calculator/imc-calculator.component";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo:"home"},
+  {path: 'home', component: HomeComponent},
+  {path: 'service-list', component: ServiceListComponent},
+  {path: 'service-list/imc-calculator', component: ImcCalculatorComponent},
+];
